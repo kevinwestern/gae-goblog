c2367bebb63c9d47c773716bd43dc07634d0ac6b
kevinwestern <kevinwestern@gmail.com>
2016-08-01 00:07:43 -0700

    Quick admin update.

--- server.go
+++ server.go
@@ -39,11 +39,22 @@ func ServeHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func ServeAdmin(w http.ResponseWriter, r *http.Request) {
+  ctx := appengine.NewContext(r)
+  u := user.Current(ctx)
+  loginOrOutUrl := ""
+  if u == nil {
+    loginOrOutUrl, _ = user.LoginURL(ctx, "/admin")
+  } else {
+    loginOrOutUrl, _ = user.LogoutURL(ctx, "/admin")
+  }
   w.Header().Set("Content-type", "text/html; charset=utf-8")
   index := template.Must(template.New("layout.html").ParseFiles(
     "templates/layout.html",
     "templates/admin.html"))
-  if err := index.ExecuteTemplate(w, "base", map[string]interface{}{}); err != nil {
+  if err := index.ExecuteTemplate(w, "base", map[string]interface{}{
+    "User": u,
+    "LoginOrOutUrl": loginOrOutUrl,
+  }); err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
   }
 }
